a33d58b898484c717077e36e03545da97aadb5db
Jean T Kasenda <jeantshimbalanga9@gmail.com>
2024-03-16 17:38:18 +0100

    Changes to network

--- Network.py
+++ Network.py
@@ -28,4 +28,35 @@ def retKML(dstip,scrip):
     except:
         return ''
     
-    
+def plotIPs(pcap):
+    kmlPts = ''
+    for (ts, buf) in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet(buf)
+            ip = eth.data
+            src = socket.inet_ntoa(ip.src)
+            dst = socket.inet_ntoa(ip.dst)
+            KML = retKML(src, dst)
+            kmlPts = kmlPts +   KML
+        except:
+            pass
+
+    return kmlPts
+
+def main():
+    f = open("16032024.pcap", "rb")
+    pcap = dpkt.pcap.Reader(f)
+    kmlheader = '<?xml version="1.0" encoding="UTF-8"?> \n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Documen>\n'\
+    '<Style id="transBluePoly">'\
+    '<LineStyle>\n'\
+    '<width>1.5</width>'\
+    '<color>5014006</color>'\
+    '</LineStyle>'\
+    '</Style>'
+    kmlfooter = '</Document>\n</kml>\n'
+    kmldoc = kmlheader+plotIPs(pcap)+kmlfooter
+    print(kmldoc)
+
+
+    if __name__ == '__main__':
+        main()
